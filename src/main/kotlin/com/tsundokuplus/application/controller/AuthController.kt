7647cfa2d1399cd761661bcd215a1a8e28da0f2a
Taku Shinohara <shinohara.mc@gmail.com>
2022-12-01 20:37:12 +0900

    feat: add API for getting a login user

--- src/main/kotlin/com/tsundokuplus/application/controller/AuthController.kt
+++ src/main/kotlin/com/tsundokuplus/application/controller/AuthController.kt
@@ -1,5 +1,8 @@
 package com.tsundokuplus.application.controller
 
+import com.tsundokuplus.application.service.UserService
+import com.tsundokuplus.application.service.security.LoginUser
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.csrf.CsrfToken
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,9 +10,24 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin
-class AuthController {
+class AuthController(
+    private val userService: UserService
+) {
     @GetMapping("/csrf-token")
     fun getCsrfToken(csrfToken: CsrfToken): CsrfToken {
         return csrfToken
     }
+
+    @GetMapping("/user")
+    fun getUser(): GetUserResponse {
+        val loginUser = SecurityContextHolder.getContext().authentication.principal as LoginUser
+        val user = userService.getUser(loginUser.email)
+        return GetUserResponse(user!!.id!!, user.email.value, user.name)
+    }
 }
+
+data class GetUserResponse(
+    val id: Int,
+    val email: String,
+    val name: String
+)
